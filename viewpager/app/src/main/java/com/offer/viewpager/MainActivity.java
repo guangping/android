b08ddf10ee34099e9e5f45c36c2d8660b411b5d5
lance <lance@lancedeMac-mini.local>
2016-04-23 16:51:41 +0800

    调整

--- viewpager/app/src/main/java/com/offer/viewpager/MainActivity.java
+++ viewpager/app/src/main/java/com/offer/viewpager/MainActivity.java
@@ -8,12 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
